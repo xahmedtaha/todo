51c7a9558a0c70d7e046b5dd124d650460d63762
Ahmed Taha <xahmedtaha930@gmail.com>
2024-07-24 10:10:58 +0300

    isDue computed prop bug fix

--- resources/js/components/Task/Task.vue
+++ resources/js/components/Task/Task.vue
@@ -9,7 +9,7 @@ defineEmits(['edit', 'delete', 'restore'])
 
 const isDue = computed(() => {
     if(props.task.due_date) {
-        return (new Date(props.task.due_date)) > Date.now()
+        return (new Date(props.task.due_date)) < Date.now()
     }
     return false
 })
@@ -25,11 +25,11 @@ const dueDate = computed(() => {
     <div class="flex justify-between gap-x-6 items-center">
         <div class="min-w-0 flex-auto">
             <p class="text-sm font-semibold leading-6 text-gray-900">
-                {{ task.title }} <span class="font-light italic text-gray-700 text-xs" :class="{isDue: 'text-red-600'}" v-if="task.due_date"> - Due {{dueDate}} </span>
+                {{ task.title }} <span class="font-light italic text-gray-700 text-xs" :class="[isDue ? 'text-red-600' : '']" v-if="task.due_date"> - Due {{dueDate}} </span>
             </p>
             <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ task.description }}</p>
         </div>
-        <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+        <div class="shrink-0 flex sm:flex-col sm:items-end">
             <div class="flex items-center gap-x-3">
                 <button @click="$emit('edit', task.id)" type="button" class="disabled:opacity-25 rounded-full text-gray-600 shadow-sm hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
                     <PencilSquareIcon class="h-5 w-5" aria-hidden="true" />
